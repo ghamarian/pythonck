c8919922b471bd59173c4d835141b60b6f4c5df4
Amir Ghamarian <amir.ghamarian@amd.com>
2025-09-18 09:17:40 +0200

    Fixes the init file issue.

--- tensor_transforms/__init__.py
+++ tensor_transforms/__init__.py
@@ -7,13 +7,16 @@ from .parser import TensorTransformParser, get_cpp_keywords, extract_descriptor_
 from .examples import get_transform_examples, get_default_variables
 from .analyzer import TensorTransformAnalyzer
 from .extract_descriptors import extract_descriptors_from_text
+from .graph_builder import build_lower_to_upper_graph, build_upper_to_lower_graph
 
 __all__ = [
     'TensorTransformParser',
     'get_cpp_keywords',
     'extract_descriptor_references',
-    'get_transform_examples', 
+    'get_transform_examples',
     'get_default_variables',
     'TensorTransformAnalyzer',
-    'extract_descriptors_from_text'
+    'extract_descriptors_from_text',
+    'build_lower_to_upper_graph',
+    'build_upper_to_lower_graph'
 ] 
